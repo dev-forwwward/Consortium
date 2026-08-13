189dc06325cc5d6362903eae35a9977a23712e75
beatriz-araujo <beatriz@forwwward.studio>
2026-08-13 17:00:34 +0100

    Added color handler for fixed border bottom elements

--- js/navbar-color-handler.js
+++ js/navbar-color-handler.js
@@ -1,56 +1,107 @@
 export function navbarColorHandler() {
     const navbar = document.querySelector('.navbar');
-    if(navbar) {
+    const border = document.querySelector('.border-bottom-el-container-inner');
+
+    if (navbar) {
 
         const lightSections = document.querySelectorAll('.light-section');
         const darkSections = document.querySelectorAll('.dark-section');
 
-        if(lightSections.length > 0) {
+        if (lightSections.length > 0) {
             lightSections.forEach((section) => {
                 ScrollTrigger.create({
                     trigger: section,
                     start: 'top top',
                     end: `bottom ${navbar.offsetHeight}px`,
-                    markers: true,
-                    onEnter: ()=> {
+                    onEnter: () => {
                         navbar.classList.add('text-color-primary');
                     },
-                    onLeave: ()=> {
+                    onLeave: () => {
                         navbar.classList.remove('text-color-primary');
                     },
-                    onEnterBack: ()=> {
+                    onEnterBack: () => {
                         navbar.classList.add('text-color-primary');
                     },
-                    onLeaveBack: ()=> {
+                    onLeaveBack: () => {
                         navbar.classList.remove('text-color-primary');
                     }
                 });
             });
         }
 
-        if(darkSections.length > 0) {
+        if (darkSections.length > 0) {
             darkSections.forEach((section) => {
                 ScrollTrigger.create({
                     trigger: section,
                     start: 'top top',
                     end: `bottom ${navbar.offsetHeight}px`,
-                    markers: true,
-                    onEnter: ()=> {
+                    onEnter: () => {
                         navbar.classList.add('text-color-seconday');
                     },
-                    onLeave: ()=> {
+                    onLeave: () => {
                         navbar.classList.remove('text-color-secondary');
                     },
-                    onEnterBack: ()=> {
+                    onEnterBack: () => {
                         navbar.classList.add('text-color-secondary');
                     },
-                    onLeaveBack: ()=> {
+                    onLeaveBack: () => {
                         navbar.classList.remove('text-color-secondary');
                     }
                 });
             });
         }
 
+    } // if navbar
 
-    }
+    
+    if (border) {
+        const lightSections = document.querySelectorAll('.light-section');
+        const darkSections = document.querySelectorAll('.dark-section');
+
+        if (lightSections.length > 0) {
+            lightSections.forEach((section) => {
+                ScrollTrigger.create({
+                    trigger: section,
+                    start: `top ${window.innerHeight - border.offsetHeight}`,
+                    end: `bottom ${window.innerHeight - border.offsetHeight}`,
+                    markers: true,
+                    onEnter: () => {
+                        border.classList.add('text-color-primary');
+                    },
+                    onLeave: () => {
+                        border.classList.remove('text-color-primary');
+                    },
+                    onEnterBack: () => {
+                        border.classList.add('text-color-primary');
+                    },
+                    onLeaveBack: () => {
+                        border.classList.remove('text-color-primary');
+                    }
+                });
+            });
+        }
+
+        if (darkSections.length > 0) {
+            darkSections.forEach((section) => {
+                ScrollTrigger.create({
+                    trigger: section,
+                    start: `top ${window.innerHeight - border.offsetHeight}`,
+                    end: `bottom ${window.innerHeight - border.offsetHeight}`,
+                    markers: true,
+                    onEnter: () => {
+                        border.classList.add('text-color-seconday');
+                    },
+                    onLeave: () => {
+                        border.classList.remove('text-color-seconday');
+                    },
+                    onEnterBack: () => {
+                        border.classList.add('text-color-seconday');
+                    },
+                    onLeaveBack: () => {
+                        border.classList.remove('text-color-seconday');
+                    }
+                });
+            });
+        }
+    } // if border
 }
